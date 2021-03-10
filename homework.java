a3501a1941eb796bbc6b9dffcd9b2e4bee77720b
gocoder <120987078@qq.com>
2021-03-11 00:29:45 +0800

    add homework

---
+++ homework.java
@@ -0,0 +1,11 @@
+public class homework {
+	public static void main(String[] args) {
+		byte a =1;
+		byte b = 2;
+		byte tmp;
+		tmp = a;
+		a = b;
+		b = tmp;
+		System.out.println(a + " " + b);
+}
+}
