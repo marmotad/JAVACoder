b08643a2ba3e2be49da6b46205ba70f5e2903f11
v_yangfan(范杨) <v_yangfan@tencent.com>
2021-03-16 22:40:43 +0800

    add java

---
+++ src/src/git/cncf/online/day7/homework7.java
@@ -0,0 +1,48 @@
+package git.cncf.online.day7;
+
+public class homework7 {
+    private static void bignum(int[] bignum) {
+        for (byte i = 0;i < bignum.length - 1 ; i ++) {
+            for (byte j = 0; j < bignum.length - 1 - i; j++ )
+            if (bignum[j] > bignum[j + 1]) {
+                int temp = bignum[j];
+                bignum[j] = bignum[j + 1];
+                bignum[j + 1] = temp;
+            }
+        }
+        System.out.println(bignum[bignum.length - 1]);
+    }
+
+
+
+public static void main(String[] args) {
+        int[] num = {1,2,3,4,5,6,7,97,77};
+        bignum(num);
+
+        int[] ints = {1,2,3,4};
+
+        for (int i = 0 ; i < ints.length; i++) {
+            int a, b, c, d;
+            a = ints[0];
+            b = ints[1];
+            c = ints[2];
+            d = ints[3];
+
+            for (a )
+
+
+
+
+            /*
+            System.out.print(ints[i]);
+            if (i + 1 < ints.length) {
+
+                System.out.println(ints[i + 1]);
+             */
+
+            }
+        }
+
+
+}
+
