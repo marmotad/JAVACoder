e8d9efda145c955c9a1947b81c02e485aa6b6017
gocoder <120987078@qq.com>
2021-03-25 22:22:46 +0800

    add java

---
+++ src/src/git/cncf/online/day13/test.java
@@ -0,0 +1,13 @@
+package git.cncf.online.day13;
+
+public class test {
+    public static void mian(String[] args) {
+        new shape1() {
+
+            @Override
+            public void a() {
+
+            }
+        };
+    }
+}
