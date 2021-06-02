49f87e1ba3493e3a67cb492341907da91c433a6c
gocoder <120987078@qq.com>
2021-06-02 20:23:39 +0800

    add java

--- src/src/git/cncf/online/day18/Page.java
+++ src/src/git/cncf/online/day18/Page.java
@@ -11,8 +11,8 @@ public class Page {
         System.out.println("* 欢迎使用学员查询系统 *");
         System.out.println("* 1 - 查询学员 *");
         System.out.println("* 2 - 插入学员 *");
-        System.out.println("* 1 - 修改学员 *");
-        System.out.println("* 1 - 删除学员 *");
+        System.out.println("* 3 - 修改学员 *");
+        System.out.println("* 4 - 删除学员 *");
         System.out.println("* 0 - 退出系统 *");
         System.out.println("*================================*");
 
@@ -52,5 +52,32 @@ public class Page {
 
         stu = new Student(name, sex, age);
         return stu;
+    }
+
+    //获取要删除的学生姓名
+    public static String getDelStuName() {
+        System.out.println("请输入要删除的学生姓名：");
+        return new Scanner(System.in).nextLine();
+    }
+
+    //操作错误提升
+    public static void error(String error) {
+        System.out.println("操作错误原因：" + error);
+    }
+
+    //删除确认信息
+    //delStu 要删除的学员
+    public static String delStuAlert(Student delStu) {
+        String delA = "";
+        while (true){
+            System.out.printf("姓名：name = %s,sex = %s, age = %d; 将被删除 (Y/N) \n", delStu.getName(), delStu.getSex(), delStu.getAge());
+            delA = new Scanner(System.in).next();
+            switch (delA) {
+                case "y": case "Y": case "n": case "N":
+                    return delA;
+                default:
+                    continue;
+            }
+        }
     }
 }
--- src/src/git/cncf/online/day18/StuMain.java
+++ src/src/git/cncf/online/day18/StuMain.java
@@ -26,7 +26,6 @@ public class StuMain {
                 //通过学员插入界面，获取一个学生对象
                 //将学生对象插入到集合
                 //一次插入一个还是多个
-
                 //Student stu = Page.getAddStudent();
                 //stu要么是要插入的对象，要么为空，表示不继续插入了
                 while (addStu(Page.getAddStudent(), stulist));
@@ -36,7 +35,20 @@ public class StuMain {
                 System.out.println("修改");
                 break;
             case 4: // 删除
-                System.out.println("删除");;
+                //通过删除界面获取要删除的学生姓名
+                String name = Page.getDelStuName();
+                //调用删除方法，获取刚刚被删除的学员信息
+                Student delStu = delByStuName(name, stulist);
+                if (delStu != null) {
+                    //确认删除信息,获取y/n，y表示删除，n表示不删除
+                    String delA = Page.delStuAlert(delStu);
+                    //String delA = Page.delStuAlert(delStu);
+                    //如果输入为y，什么都不做，如果是n，再将刚刚删除的信息插入回去
+                    if (delA.equals("N") || delA.equals("n"))
+                        stulist.add(delStu);
+                } else {
+                    Page.error("要删除的用户不存在");
+                }
                 break;
             case 0: // 退出
                 System.out.println("欢迎再次使用！！");
@@ -44,6 +56,18 @@ public class StuMain {
                 break;
 
         }
+    }
+
+    //通过学生姓名删除成员
+    //name 要删除的学员姓名
+    //stuList 学员集合
+    //return 删除成功，返回被删除的学员对象，删除失败返回nill
+    private static Student delByStuName(String name, ArrayList<Student> stulist) {
+        for ( int i = 0 ; i < stulist.size(); i++) {
+            if (name.equals(stulist.get(i).getName()))
+                return stulist.remove(i);
+        }
+        return null;
     }
 
     //插入学员信息
